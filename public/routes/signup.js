3e3ba20fc8072fd0edce0b79e9765f2fc0150a7e
SubbaraoGarlapati <53627478+SubbaraoGarlapati@users.noreply.github.com>
2019-08-02 16:34:52 -0500

    Delete signup.js

--- public/routes/signup.js
+++
@@ -1,9 +0,0 @@
-//Creates router or mini-app
-const router = require('express').Router();
-// GET /renders signup.pug view
-router.get('/', function(req, res, next) {
-  res.render('signup');
-});
-
-//sets export of module to be router
-module.exports = router;
